efa59d9e8f40be2e5c4930f969f5aac8dae3d06f
4c <4c.mura@gmail.com>
2024-09-18 17:28:51 +0900

    modified

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from '../three/examples/jsm/controls/OrbitControls';
 
 // import * as THREE from "three"; // 修正
 // import { OrbitControls } from "./jsm/controls/OrbitControls.js";
