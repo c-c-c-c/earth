146b6df969b8b4676fd7c5aca3c2c34ddb6ce447
4c <4c.mura@gmail.com>
2024-09-18 17:13:58 +0900

    type moduleがどれくらいできるか

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-import * as THREE from "three"; // 修正
-import { OrbitControls } from "./jsm/controls/OrbitControls.js";
+// import * as THREE from "three"; // 修正
+// import { OrbitControls } from "./jsm/controls/OrbitControls.js";
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-(() => {
+// (() => {
 
 
 
-})();
+// })();
 
 
 console.log("1");
@@ -23,35 +23,35 @@ console.log("1");
 
 
 // サイズを指定
-const width = 960;
-const height = 540;
+// const width = 960;
+// const height = 540;
 
-// レンダラーを作成
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#myCanvas"),
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(width, height);
+// // レンダラーを作成
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: document.querySelector("#myCanvas"),
+// });
+// renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.setSize(width, height);
 
-// シーンを作成
-const scene = new THREE.Scene();
+// // シーンを作成
+// const scene = new THREE.Scene();
 
-// カメラを作成
-const camera = new THREE.PerspectiveCamera(45, width / height);
-camera.position.set(0, 0, +1000);
+// // カメラを作成
+// const camera = new THREE.PerspectiveCamera(45, width / height);
+// camera.position.set(0, 0, +1000);
 
-// 箱を作成
-const geometry = new THREE.BoxGeometry(400, 400, 400);
-const material = new THREE.MeshNormalMaterial();
-const box = new THREE.Mesh(geometry, material);
-scene.add(box);
+// // 箱を作成
+// const geometry = new THREE.BoxGeometry(400, 400, 400);
+// const material = new THREE.MeshNormalMaterial();
+// const box = new THREE.Mesh(geometry, material);
+// scene.add(box);
 
-tick();
+// tick();
 
-// 毎フレーム時に実行されるループイベントです
-function tick() {
-  box.rotation.y += 0.01;
-  renderer.render(scene, camera); // レンダリング
+// // 毎フレーム時に実行されるループイベントです
+// function tick() {
+//   box.rotation.y += 0.01;
+//   renderer.render(scene, camera); // レンダリング
 
-  requestAnimationFrame(tick);
-}
+//   requestAnimationFrame(tick);
+// }
